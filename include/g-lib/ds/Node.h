a4074965152037e3a090eeca428b8017b4fcaa3b
Paul Galasti <pgalasti@gmail.com>
2025-12-05 12:32:12 -0500

    Added double ended node struct

--- include/g-lib/ds/Node.h
+++ include/g-lib/ds/Node.h
@@ -18,4 +18,27 @@ namespace GLib::DS {
     attach_node(pRoot->pNextNode, pNode);
     return pRoot;
   }
+
+  template<typename T>
+  struct DNode {
+    T value;
+    DNode* pLeft;
+    DNode* pRight;
+  };
+  
+  // Usage:
+  // pRoot = attachNode(pRoot, pNewNode, [](const int& a, const int& b) { return a < b; });
+  template<typename T, typename Comp>
+  DNode<T>* attach_node(DNode<T>* pRoot, DNode<T>* pNode, Comp comFn)
+  {
+    if(!pRoot) { return pNode; }
+
+    if(comFn(pNode->value, pRoot->value)) {
+        pRoot->pLeft = attach_node(pRoot->pLeft, pNode, comFn);
+	return pRoot;
+    }
+
+    pRoot->pRight = attach_node(pRoot->pRight, pNode, comFn);
+    return pRoot;
+  }
 }
