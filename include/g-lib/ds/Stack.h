f367b4289c892c069adbdb04e3ce8d6f954fe839
Paul Galasti <pgalasti@gmail.com>
2025-12-06 19:10:39 -0500

    Stack structure definition. Implementation needed

---
+++ include/g-lib/ds/Stack.h
@@ -0,0 +1,33 @@
+#ifndef GLIB_STACK_H
+#define GLIB_STACK_H
+
+#include "Node.h"
+
+namespace GLib::DS {
+
+  template <typename T>
+  class Stack {
+    private:
+      unsigned int    m_Size;
+      SNode<T>*       m_pTop;
+
+    public:
+      Stack();
+      ~Stack();
+      Stack           (Stack &&other) noexcept;
+      Stack           (const Stack&) = delete;
+      Stack &operator=(const Stack&) = delete;
+      Stack &operator=(Stack &&other) noexcept;
+
+      void            Push(const T& t);
+      T               Pop();
+      T               Peek()    const;
+      unsigned int    Size()    const;
+      bool            IsEmpty() const;
+      void            Clear();
+
+  }
+}
+
+
+#endif
