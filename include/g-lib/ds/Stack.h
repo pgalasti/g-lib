776928663312565de76884593c8bdca3a69470db
Paul Galasti <pgalasti@gmail.com>
2025-12-07 10:35:53 -0500

    Stack implementation. Need tests

--- include/g-lib/ds/Stack.h
+++ include/g-lib/ds/Stack.h
@@ -5,29 +5,26 @@
 
 namespace GLib::DS {
 
-  template <typename T>
-  class Stack {
-    private:
-      unsigned int    m_Size;
-      SNode<T>*       m_pTop;
+template <typename T> class Stack {
+private:
+  unsigned int m_Size;
+  SNode<T> *m_pTop;
 
-    public:
-      Stack();
-      ~Stack();
-      Stack           (Stack &&other) noexcept;
-      Stack           (const Stack&) = delete;
-      Stack &operator=(const Stack&) = delete;
-      Stack &operator=(Stack &&other) noexcept;
+public:
+  Stack();
+  ~Stack();
+  Stack(Stack &&other) noexcept;
+  Stack(const Stack &) = delete;
+  Stack &operator=(const Stack &) = delete;
+  Stack &operator=(Stack &&other) noexcept;
 
-      void            Push(const T& t);
-      T               Pop();
-      T               Peek()    const;
-      unsigned int    Size()    const;
-      bool            IsEmpty() const;
-      void            Clear();
-
-  }
-}
-
+  void Push(const T &t);
+  T Pop();
+  T Peek() const;
+  unsigned int Size() const;
+  bool IsEmpty() const;
+  void Clear();
+};
+} // namespace GLib::DS
 
 #endif
---
+++ src/ds/Stack.cpp
@@ -0,0 +1,85 @@
+#include "g-lib/ds/Stack.h"
+
+#include <stdexcept>
+
+namespace GLib::DS {
+
+template <typename T> Stack<T>::Stack() : m_Size(0), m_pTop(nullptr) {
+}
+
+template <typename T> Stack<T>::~Stack() {
+  Clear();
+}
+
+template <typename T> Stack<T>::Stack(Stack &&other) noexcept {
+  m_Size = other.m_Size;
+  m_pTop = other.m_pTop;
+
+  other.m_Size = 0;
+  other.m_pTop = nullptr;
+}
+
+template <typename T> Stack<T> &Stack<T>::operator=(Stack &&other) noexcept {
+  if (this == &other) {
+    return *this;
+  }
+
+  Clear();
+
+  m_Size = other.m_Size;
+  m_pTop = other.m_pTop;
+
+  other.m_Size = 0;
+  other.m_pTop = nullptr;
+
+  return *this;
+}
+
+template <typename T> void Stack<T>::Push(const T& value) {
+
+  ++m_Size;
+  
+  SNode<T>* pNewNode = new SNode<T>();
+  pNewNode->value = value;
+  pNewNode->pNextNode = m_pTop;
+
+  m_pTop = pNewNode;
+}
+
+template <typename T> T Stack<T>::Pop() {
+
+  if(IsEmpty()) {
+    throw std::runtime_error("Stack is empty. Unable to pop!");
+  }
+  
+  --m_Size;
+
+  const T value = m_pTop->value;
+  SNode<T>* pCursor = m_pTop->pNextNode;
+  delete m_pTop;
+
+  m_pTop = pCursor;
+
+  return value; 
+}
+
+template <typename T> T Stack<T>::Peek() const {
+  
+  if(IsEmpty()) {
+    throw std::runtime_error("Stack is empty. Unable to peek!");
+  }
+
+  return m_pTop->value;
+}
+
+template <typename T> unsigned int Stack<T>::Size() const { return m_Size; }
+
+template <typename T> bool Stack<T>::IsEmpty() const { return m_Size == 0; }
+
+template <typename T> void Stack<T>::Clear() {
+  while(m_pTop) {
+    Pop();
+  }
+}
+
+} // namespace GLib::DS
